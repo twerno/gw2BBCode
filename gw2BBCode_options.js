3a45ae2eab451ffe4f343543aa74f7b1482c09ef
twerno <twerno@gmail.com>
2012-09-25 21:02:47 +0200

    [FIX] links to resources

--- gw2BBCode_options.js
+++ gw2BBCode_options.js
@@ -10,10 +10,10 @@
 	
 	var incrementalSearch = true;
 	
-	var gw2BBCode_img_host = "gw2_images";
+	var gw2BBCode_img_host = "https://s3-eu-west-1.amazonaws.com/gw2bbcode.pl/gw2_images";
 	var gw2DB_Url          = "http://www.gw2db.com";
 	var wiki_Url           = "http://wiki.guildwars2.com/wiki";
 	var gw2DB_PopupHost    = "http://www.gw2db.com/{0}/{1}/tooltip?x&advanced=1&callback=?";
 	//var popup_style      = "http://static-ascalon.cursecdn.com/current/skins/Ascalon/css/tooltip.css";
-	var popup_style        = "tooltip.css";
+	var popup_style        = "https://s3-eu-west-1.amazonaws.com/gw2bbcode.pl/tooltip.css";
 	//var popup_style      = "../dist/tooltip.css";
