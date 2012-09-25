4540078a944d6d241dc8fc2cf94e7ab2f8d96844
twerno <twerno@gmail.com>
2012-09-25 19:24:00 +0200

    moved more consts to options file

--- gw2BBCode_core.js
+++ gw2BBCode_core.js
@@ -1,11 +1,5 @@
 
-	var gw2DBMap        = {};
-	var gw2DB_Url       = "http://www.gw2db.com";
-	var wiki_Url        = "http://wiki.guildwars2.com/wiki";
-	var gw2DB_PopupHost = "http://www.gw2db.com/{0}/{1}/tooltip?x&advanced=1&callback=?";
-	//var popup_style     = "http://static-ascalon.cursecdn.com/current/skins/Ascalon/css/tooltip.css";
-	var popup_style     = "https://s3-eu-west-1.amazonaws.com/gw2bbcode.pl/tooltip.css";
-	//var popup_style     = "../dist/tooltip.css";
+	var gw2DBMap = {};
 	
 	function init() {
 		$(includeTo.join(',')).addClass('gw2BBCode');
@@ -263,6 +257,8 @@
 	function initGoogleAnalitics() {
 		_gaq.push(
 			['x._setAccount', 'UA-35005283-2'],
+			['x._setAllowLinker', true],
+			['x._setDomainName', 'none'],
 			['x._trackPageview']);
 	
 		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
